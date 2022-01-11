4e27e3e9d6a5e05f111720683bfb519580b03ec5
yilmazelif97 <elifyilmaz587@gmail.com>
2022-01-11 12:39:58 +0300

    async

---
+++ asenkronyapi.js
@@ -0,0 +1,57 @@
+//senkron yapıda bir iş bitmeden diğer işe bağlanmaz
+//asenkron yapıda aynı anda 1den fazla iş yapılabilir
+
+// console.log('1. görev');
+// console.log('2. görev');
+// console.log('3. görev');
+
+
+//kodun bulunduğu satır sayısına gçre çalışmasına gerek yok kod sırasına gööre çalışabilir. 
+
+/*const func1=()=>{
+    console.log("func 1 tamamlandı");
+};
+
+const func2=()=>{
+    console.log("func 2 tamamlandı");
+};
+
+func2();
+func1(); */
+
+// const func1=()=>{
+//     console.log("func 1 tamamlandı");
+//     func2();
+// };
+
+// const func2=()=>{
+//     console.log("func 2 tamamlandı");
+//     func3();
+// };
+
+// const func3=()=>{
+//     console.log("func 3 tamamlandı");
+// };
+
+// const func1=()=>{
+//     console.log("func 1 tamamlandı");
+//     func3();
+
+// };
+
+// const func2=()=>{
+//     console.log("func 2 tamamlandı");
+    
+// };
+
+// const func3=()=>{
+//     console.log("func 3 tamamlandı");
+//     func2()
+// };
+
+// func1(); 
+
+// bu çalışma yapısı senkron yapıdadır. Javascriptin yapısı senkrondur -> single thread 
+
+
+
