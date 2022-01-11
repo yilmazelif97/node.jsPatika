bf99979921a4632ec99d0fc68b98a9dbf2566560
yilmazelif97 <elifyilmaz587@gmail.com>
2022-01-11 12:08:12 +0300

    odev1

---
+++ odev1.js
@@ -0,0 +1,12 @@
+
+function findArea(yarıcap){
+
+    const pi =3.14;
+    var area = pi*yarıcap*yarıcap;
+
+    console.log(`Yarıçapı ${yarıcap} olan dairenin alani: ${area} `);
+
+}
+
+findArea(2);
+
