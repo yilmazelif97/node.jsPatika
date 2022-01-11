06a47edeef911704c02f951116228bb3e3b24895
yilmazelif97 <elifyilmaz587@gmail.com>
2022-01-11 12:06:59 +0300

    asalsayi and odev1 created

---
+++ asalsayibul.js
@@ -0,0 +1,28 @@
+
+function showPrimeNumber(lownumber, highnumber){
+
+    for(let i =lownumber; i<=highnumber;i++){
+        
+        let isPrime  =true; //tüm sayılar asal gibi
+        
+        for(let j =2;j<=i;j++){
+
+            if(i%j===0 && j!==i){
+                isPrime=false;
+            }
+
+        }
+
+        if(isPrime){
+            console.log(i);
+        }
+    }
+
+}
+
+showPrimeNumber(14,40); //manuel olarak bu değerler giriliyor.
+
+//console.log(process); //node.js e ait global nesnelerden bir tanesi. Bize o anki işlemle ilgili bilgiler verir
+
+//console.log(process.argv); //projeyi çalıştırırken kullanılan paramtelreli içeren array
+
